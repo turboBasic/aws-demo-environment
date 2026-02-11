c902dbc1c512217ccc79adf763fce886d17ed574
turboBasic ⚡️💀 <andrii7melnyk@proton.me>
2026-02-11 03:58:37 +0100

    feat(main): add backend configuration
    Populate backend config with values generated by booststrap

--- backend.tf
+++ backend.tf
@@ -1,12 +1,12 @@
 # S3 remote state backend. Fill in values from bootstrap outputs:
 #   terraform output -json backend_config
-#
-# terraform {
-#   backend "s3" {
-#     bucket         = "<bootstrap: state_bucket_name>"
-#     key            = "aws-demo/terraform.tfstate"
-#     region         = "eu-central-1"
-#     dynamodb_table = "<bootstrap: dynamodb_table_name>"
-#     encrypt        = true
-#   }
-# }
+
+terraform {
+  backend "s3" {
+    bucket         = "aws-demo-demo-tfstate-381492075850"
+    key            = "aws-demo/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "aws-demo-demo-tflock"
+    encrypt        = true
+  }
+}
