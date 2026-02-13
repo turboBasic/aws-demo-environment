2f3511bb421c683c93878bd58834c5d2e670143d
turboBasic ⚡️💀 <andrii7melnyk@proton.me>
2026-02-13 14:29:24 +0100

    feat(cloudflare): redirect www subdomain to apex

--- certificates.tf
+++ certificates.tf
@@ -39,3 +39,42 @@ resource "cloudflare_zone_setting" "ssl" {
   setting_id = "ssl"
   value      = "strict"
 }
+
+# www redirect to apex
+resource "cloudflare_dns_record" "www" {
+  zone_id = var.cloudflare_zone_id
+  name    = "www.${var.domain_name}"
+  content = aws_lb.demo.dns_name
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true # must be proxied for redirect rules to work
+}
+
+# In order to allow editing rulesets, the Cloudflare API token must have the following permissions:
+# Account - Account Rulesets:Edit
+# Zone - Single Redirect:Edit, Zone Settings:Edit, DNS:Edit
+resource "cloudflare_ruleset" "redirect_www" {
+  zone_id     = var.cloudflare_zone_id
+  name        = "Redirect www to apex"
+  description = "Redirect www.domain.com to domain.com"
+  kind        = "zone"
+  phase       = "http_request_dynamic_redirect"
+
+  rules = [{
+    ref         = "redirect_www_to_apex"
+    action      = "redirect"
+    expression  = "(http.host eq \"www.${var.domain_name}\")"
+    description = "Redirect www to apex"
+
+    action_parameters = {
+      from_value = {
+        status_code           = 301
+        preserve_query_string = true
+
+        target_url = {
+          value = "https://${var.domain_name}/"
+        }
+      }
+    }
+  }]
+}
